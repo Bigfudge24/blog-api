f752e740197909d50167676173e9d9870b445e8c
beugfudge <tomoyaa@tutamail.com>
2023-02-03 14:16:43 +0530

    added model mapper

--- src/main/java/com/blog/blogappapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/blog/blogappapi/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.blog.blogappapi.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,8 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -56,21 +59,18 @@ public class UserServiceImpl implements UserService{
         this.userRepository.delete(user);
     }
     public User dtoToUser(UserDto userDto){
-        User user=new User();
-        user.setuId(userDto.getuId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-        user.setAbout(userDto.getAbout());
+        User user=this.modelMapper.map(userDto, User.class);
+
+        // user.setuId(userDto.getuId());
+        // user.setName(userDto.getName());
+        // user.setEmail(userDto.getEmail());
+        // user.setPassword(userDto.getPassword());
+        // user.setAbout(userDto.getAbout());
         return user;
     }
     public UserDto usertoDto(User user){
-        UserDto userDto=new UserDto();
-        userDto.setuId(user.getuId());
-        userDto.setName(user.getName());
-        userDto.setEmail(user.getEmail());
-        userDto.setPassword(user.getPassword());
-        userDto.setAbout(user.getAbout());
+        UserDto userDto=this.modelMapper.map(user, UserDto.class);
+        
         return userDto;
     }
     
