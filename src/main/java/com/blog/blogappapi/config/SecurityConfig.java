56d4bf2bcb75faca82f98badd454d07a8bc468e2
beugfudge <tomoyaa@tutamail.com>
2023-04-06 13:54:33 +0530

    added security config

--- src/main/java/com/blog/blogappapi/config/SecurityConfig.java
+++ src/main/java/com/blog/blogappapi/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.blog.blogappapi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.blog.blogappapi.repository.UserRepository;
 // import com.blog.blogappapi.security.CustomUserDetailService;
@@ -24,7 +26,16 @@ import com.blog.blogappapi.security.JwtAuthenticationFilter;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@EnableWebMvc
 public class SecurityConfig {
+	public static final String[] PUBLIC_URLS={
+		"/api/v1/auth/**",
+		"/v3/api-docs",
+		"/v2/api-docs",
+		"/swagger-resources/**",
+		"/swagger-ui/**",
+		"/webjars/**"
+	};
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
 	@Autowired
@@ -56,7 +67,8 @@ public class SecurityConfig {
 		http
 			.csrf().disable()
 			.authorizeHttpRequests()
-			.requestMatchers("/api/v1/auth/**").permitAll()
+			.requestMatchers(PUBLIC_URLS).permitAll()
+			.requestMatchers(HttpMethod.GET).permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.sessionManagement()
